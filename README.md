885be3aea85a5892740064c9fb5c770677c782e6
Rifat <tmrifat87@gmail.com>
2025-07-31 23:18:23 +0600

    Sending Contract setup

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ DApp: Blockchian
 - that has the following button which map to the solidity smart contract
     - Connect
     - Buy Coffee
+        - Button to buy coffee
+        -  Call a function on a smart contract
+        - have a test blockchian that we can call?
     - Get Balance
     - Withdraw
--- index-js.js
+++ index-js.js
@@ -1,1 +1,62 @@
+import { createWalletClient, custom, createPublicClient } from "https://esm.sh/viem";
+
 const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const ethAmountInput = document.getElementById("ethAmount");
+
+let walletClient;
+let publicClient
+
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Welcome");
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+
+        // Request accounts if needed from MetaMask
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        // Now get the addresses via viem
+        const addresses = await walletClient.getAddresses();
+        console.log('Connected addresses:', addresses);
+
+        connectButton.innerHTML = "Connected: " + addresses[0];
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!";
+    }
+}
+
+async function fund() {
+    const ethAmount = ethAmountInput.value;
+    console.log(`Funding with ${ethAmount}...`)
+
+
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Welcome");
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+
+        // Request accounts if needed from MetaMask
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+
+        await publicClient.simulateContract({
+            // address ???
+        })
+
+        // Now get the addresses via viem
+        const addresses = await walletClient.getAddresses();
+        console.log('Connected addresses:', addresses);
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!";
+    }
+}
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
